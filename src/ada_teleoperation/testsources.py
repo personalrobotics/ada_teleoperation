e6396f8f6cc0e642d4e6a964196cf54c31d182bf
Pyry Matikainen <pmatikai@ada.personalrobotics.ri.cmu.edu>
2016-11-11 17:35:50 -0500

    Seems to be working kinda?

---
+++ src/ada_teleoperation/testsources.py
@@ -0,0 +1,34 @@
+import numpy as np
+from RobotState import Action
+import math
+
+class MoveUpTestSource:
+    def __init__(self, move_amount=0.05):
+        self.move_amount = move_amount
+        self.target = None
+
+    def get_action(self, robot, robot_state):
+        ret = Action()
+        if self.target is None:
+            self.target = robot_state.ee_trans.copy()
+            self.target[0,3] += self.move_amount
+        ret.twist_from_transform(robot.arm, self.target)
+        return ret
+
+class SineTestSource:
+    def __init__(self, move_amount=0.05, frequency=1.0):
+        self.move_amount = move_amount
+        self.target = None
+        self.frequency = frequency
+        self.t = 0.0
+        self.z0 = 0.0
+
+    def get_action(self, robot, robot_state):
+        self.t += 1.0/60.0
+        ret = Action()
+        if self.target is None:
+            self.target = robot_state.ee_trans.copy()
+            self.z0 = self.target[0,3]
+        self.target[0,3] = self.z0 + math.sin(self.t*self.frequency)*self.move_amount
+        ret.twist_from_transform(robot.arm, self.target)
+        return ret
